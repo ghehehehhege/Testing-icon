f9fac19c4339562ccf15e7b46b763face6659b0a
ALEX <122011254+ghehehehhege@users.noreply.github.com>
2024-06-09 23:59:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
-from flask import Flask, send_from_directory, abort, jsonify
+from flask import Flask, send_file, abort
 import os
 
 app = Flask(__name__)
-icon_directory = os.path.dirname(os.path.abspath(__file__))
-
-@app.route('/', methods=['GET'])
-def forbidden():
-    """Handle requests to the root URL and return 403 Forbidden."""
-    return jsonify({"error": "Forbidden"}), 403
+current_directory = os.path.dirname(os.path.abspath(__file__))
+icon_path = os.path.join(current_directory, "Icon_Emote_LOL.png")
 
 @app.route('/api/icon/<filename>', methods=['GET'])
 def get_icon(filename):
-    """Serve an individual icon by filename if it exists."""
-    try:
-        file_path = os.path.join(icon_directory, filename)
-        if os.path.isfile(file_path) and filename.endswith(('.png', '.jpg', '.jpeg', '.gif', '.svg')):
-            return send_from_directory(icon_directory, filename)
-        else:
-            return jsonify({"error": "File not found"}), 404
-    except Exception as e:
-        print(f"Error: {e}")
-        return jsonify({"error": "Internal server error"}), 500
+    """Serve the Icon_Emote_LOL.png file."""
+    if filename == 'Icon_Emote_LOL.png':
+        return send_file(icon_path, mimetype='image/png')
+    else:
+        abort(404)
 
+if __name__ == '__main__':
+    app.run(debug=True)
