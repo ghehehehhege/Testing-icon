6f80e5717a9e8e822a48ddd41e46c9acda090687
ALEX <122011254+ghehehehhege@users.noreply.github.com>
2024-06-09 23:40:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory, abort
+from flask import Flask, send_from_directory, abort, jsonify
 import os
 
 app = Flask(__name__)
@@ -7,11 +7,15 @@ icon_directory = os.path.dirname(os.path.abspath(__file__))
 @app.route('/api/icon/<filename>', methods=['GET'])
 def get_icon(filename):
     """Serve an individual icon by filename if it exists."""
-    file_path = os.path.join(icon_directory, filename)
-    if os.path.isfile(file_path) and filename.endswith(('.png', '.jpg', '.jpeg', '.gif', '.svg')):
-        return send_from_directory(icon_directory, filename)
-    else:
-        abort(404, description="Resource not found")
+    try:
+        file_path = os.path.join(icon_directory, filename)
+        if os.path.isfile(file_path) and filename.endswith(('.png', '.jpg', '.jpeg', '.gif', '.svg')):
+            return send_from_directory(icon_directory, filename)
+        else:
+            return jsonify({"error": "File not found"}), 404
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({"error": "Internal server error"}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
